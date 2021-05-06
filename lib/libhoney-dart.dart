5f47e17695ea25b14a2c339aaae23a6a2d3a230c
One_Nub <38899321+One-Nub@users.noreply.github.com>
2021-05-05 21:26:21 -0500

    Revert "Create base Libhoney and Event classes + libhoney package"
    This reverts commit bef175a3b7ee2bb58f3ea9caa297a0e4ab317ce1.

--- lib/libhoney-dart.dart
+++
@@ -1,7 +0,0 @@
-/// Client library for honeycomb.io for Dart
-/// 
-/// Main library which contains all necessary things to interact with honeycomb.io
-library libhoney;
-
-part 'src/libhoney.dart';
-part 'src/event.dart';
