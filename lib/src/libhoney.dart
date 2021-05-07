26916ade40257f42357ba013a3f9d33dc2a30efd
One_Nub <38899321+One-Nub@users.noreply.github.com>
2021-05-07 16:16:27 -0500

    Validate events, disable sending, instantiate transmission

--- lib/src/libhoney.dart
+++ lib/src/libhoney.dart
@@ -9,16 +9,47 @@ class Libhoney {
   String? dataset;
   String? writeKey;
   int sampleRate;
+  bool disabled;
+
+  Transmission? _transmission;
 
   Map _globalFields = {};
-  
-  Libhoney({this.apiHost = _apiHost, dataset, writeKey, this.sampleRate = 1});
+
+  Libhoney(
+      {this.apiHost = _apiHost,
+      dataset,
+      writeKey,
+      this.sampleRate = 1,
+      this.disabled = false}) {
+    if (!disabled) _transmission = Transmission();
+  }
 
   Future<void> sendEvent(Event event) async {
-    //TODO: Validate event contents via method
+    Event validatedEvent = _validateEvent(event);
+    if(_transmission != null) {
+      _transmission!.send(validatedEvent);
+    }
   }
 
   void addGlobalField(String key, Object val) {
     _globalFields[key] = val;
+  }
+
+  Event _validateEvent(Event event) {
+    Event validatedEvent = event;
+
+    if (event.apiHost == null || event.apiHost!.isEmpty) throw MissingApiHost();
+
+    if (event.dataset == null || event.dataset!.isEmpty) throw MissingDatasetName();
+
+    if (event.writeKey == null || event.writeKey!.isEmpty) throw MissingWritekey();
+
+    validatedEvent.sampleRate ??= 1;
+
+    validatedEvent.timestamp ??= DateTime.now();
+
+    if (event._fields.isEmpty) throw MissingData();
+
+    return validatedEvent;
   }
 }
