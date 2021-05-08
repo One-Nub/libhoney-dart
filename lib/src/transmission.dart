e546668974f35744348709063a4a0a0c350a881d
One_Nub <38899321+One-Nub@users.noreply.github.com>
2021-05-08 00:54:05 -0500

    Add documentation for Transmission class.

--- lib/src/transmission.dart
+++ lib/src/transmission.dart
@@ -1,34 +1,47 @@
 part of '../libhoney-dart.dart';
 
+/// Useragent for the library.
 const _userAgent = "libhoney-dart/$_version";
+
+/// Default content type for the library.
 const _contentType = "application/json";
 
-
+/// Manages all sending of [Event]s.
 class Transmission {
+  /// Creates new Transmisson handler.
   Transmission();
 
+  /// Sends an event.
+  ///
+  /// Requires an [event] to send. Utilizes values set on the event to then send upstream.
   Future<void> send(Event event) async {
     //Stop if event is not to be sent
     if(!_sampleEvent(event.sampleRate!)) return;
 
     Uri url = Uri.parse("${event.apiHost!}$_eventEndpoint/${event.dataset}");
     var headers = _generateHeaders(event);
-    
+
     http.Response resp = await http.post(url, headers: headers, body: json.encode(event._fields));
 
-    //Since responses are not implemented yet - I wish to utilize the logging
-    //package for this for the time being.
+    // TODO: Log the response with the logging package
+    // (?) Until responses are implemented. Or integrate a toggle for print logging.
     print(resp);
   }
 
+  /// Performs sampling on an event.
+  ///
+  /// When false is returned, the event will not be sent.
   bool _sampleEvent(int sampleRate) {
     if(sampleRate <= 1) return true;
     return Random.secure().nextDouble() < 1 / sampleRate;
   }
 
+  /// Generates the headers required for post request in [send()]
+  ///
+  /// Utilizes variables set on event to create required headers.
   Map<String, String> _generateHeaders(Event event) {
     Map<String, String> headers = {};
-    
+
     headers["X-Honeycomb-Team"] = event.writeKey!;
     headers["Content-Type"] = _contentType;
     headers["User-Agent"] = _userAgent;
