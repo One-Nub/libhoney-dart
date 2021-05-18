cf4903eec8a8d5df30d21ebde5b086f92ed07ef3
One_Nub <38899321+One-Nub@users.noreply.github.com>
2021-05-18 02:22:41 -0500

    Reorder vars & reveal raw response

--- lib/src/event_response.dart
+++ lib/src/event_response.dart
@@ -1,20 +1,11 @@
 part of '../libhoney-dart.dart';
 
 class EventResponse {
-  /// Resulting status code of sent event.
-  late int statusCode;
-
   /// Response body of the request.
   late String body;
 
   /// Original Event that was sent.
   Event event;
-
-  /// Time in milliseconds it took for the event to send.
-  int submitDuration;
-
-  /// [Event.metadata], used to identify the event.
-  dynamic? metadata;
 
   /// Error that the library encountered while sending the event.
   /// 
@@ -22,9 +13,21 @@ class EventResponse {
   /// upon validating, this will be populated.
   String? libError;
 
-  EventResponse(Response response, this.event, this.submitDuration, {this.libError}) {
-    statusCode = response.statusCode;
-    body = response.body;
+  /// [Event.metadata], used to identify the event.
+  dynamic? metadata;
+
+  /// Raw response from post request sent upstream.
+  Response rawResponse;
+
+  /// Resulting status code of sent event.
+  late int statusCode;
+
+  /// Time in milliseconds it took for the event to send.
+  int submitDuration;
+
+  EventResponse(this.rawResponse, this.event, this.submitDuration, {this.libError}) {
+    statusCode = rawResponse.statusCode;
+    body = rawResponse.body;
     metadata = event.metadata;
   }
 }
