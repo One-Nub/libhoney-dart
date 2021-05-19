14d9edb2abddc1c17d763652a699e30165a35dd0
One_Nub <38899321+One-Nub@users.noreply.github.com>
2021-05-18 23:20:57 -0500

    Add simple example file

---
+++ example/example.dart
@@ -0,0 +1,17 @@
+import "package:libhoney_dart/libhoney-dart.dart";
+
+// Main function
+void main() {
+  Libhoney honey = Libhoney(writeKey: "API_KEY", dataset: "your_dataset");
+
+  Event event = Event(honey);
+  event.addField("user ID", 4829034023923094);
+  event.addMap({
+    "access": "member", 
+    "crashed": false
+  });
+  event.metadata = "EVENT_1";
+
+  // Awaiting this will force the code to wait for the response in honey.responseQueue
+  event.send();
+}
